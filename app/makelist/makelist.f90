f9f933e49b717db2f2f072ad99a7143638296c05
David A. Saunders <David.A.Saunders@NASA.gov>
2021-03-09 23:03:33 -0800

    Send the list to file unless it is short enough for a cut and paste.
    The makelist.f original is replaced with makelist.f90.

--- app/makelist/makelist.f90
+++ app/makelist/makelist.f90
@@ -17,3 +17,29 @@
 !                     Now with AMA, Inc. at NASA Ames.
 !
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+   implicit none
+
+   integer :: i, i1, i2, inc, l, lun, ndigits, nlist
+   character (4) cformat
+
+   cformat = '(in)'
+   write (*, '(/, a)') ' Enter i1, i2, inc: '
+   read  (*, *) i1, i2, inc
+
+   l = max (abs (i1), abs (i2))
+   ndigits = min (max (1, int (alog10 (real (10*l)))), 9)
+   if (i1 < 0 .or. i2 < 0) ndigits = ndigits + 1  ! Allow for the -ve sign
+   write (cformat(3:3), '(i1)') ndigits
+   lun = 6
+   nlist = nint (real (i2 - i1) / real (inc)) + 1
+
+   if (nlist > 100) then
+      lun = 1
+      open (lun, file='ordinals.dat', status='unknown')
+   end if
+
+   write (6, '(a)') cformat
+   write (lun, cformat) (i, i = i1, i2, inc)
+
+   end program makelist
